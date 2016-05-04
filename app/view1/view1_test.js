808287d04d9cbb3f39678ccddb03db538281af4b
Lucas Cherkewski <lucas@ecustom.ca>
2016-05-04 17:01:23 -0400

    fix references to myApp

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -1,8 +1,8 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('courseComparator.view1 module', function() {
 
-  beforeEach(module('myApp.view1'));
+  beforeEach(module('courseComparator.view1'));
 
   describe('view1 controller', function(){
 
