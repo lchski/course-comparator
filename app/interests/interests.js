c956c22be65533adabd6d091cd9c834cadbd480d
Lucas Cherkewski <lucas@ecustom.ca>
2016-05-07 11:49:18 -0400

    write up InterestsCtrl a bit

--- app/interests/interests.js
+++ app/interests/interests.js
@@ -1,6 +1,10 @@
 'use strict';
 
-angular.module('courseComparator.interests', ['ngRoute'])
+angular.module('courseComparator.interests', ['ngRoute', 'LocalStorageModule'])
+
+    .config(['localStorageServiceProvider', function (localStorageServiceProvider) {
+        localStorageServiceProvider.setPrefix('uocc');
+    }])
 
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/interests', {
@@ -9,6 +13,18 @@ angular.module('courseComparator.interests', ['ngRoute'])
         });
     }])
 
-    .controller('InterestsCtrl', ['$scope', function($scope) {
-        
+    .controller('InterestsCtrl', ['$scope', 'localStorageService', function($scope, localStorageService) {
+        var interestsInStore = localStorageService.get('interests');
+
+        $scope.interests = interestsInStore || [];
+
+        $scope.$watch('interests', function () {
+            localStorageService.set('interests', $scope.interests);
+        }, true);
+
+        $scope.removeInterest = function(courseCode, e) {
+            e.preventDefault();
+
+            $scope.interests = _($scope.interests).without(_($scope.interests).findWhere({code: courseCode}));
+        };
     }]);
